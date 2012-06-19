706e0af85b21743dde40b4a12755ac486f2df409
Nana Kwesi Baffoe-Mensah <NanaK@Nanas-MacBook-Pro-2.local>
2012-06-19 16:39:38 +0000

    python stage 1 update

--- Day1.py
+++ Day1.py
@@ -7,10 +7,45 @@ print'Hello World'
 print 11/2
 
 #question 1 correct answer
+print 'question 1'
 print 11.0/2
 
 #question 2  exponentials
+print 'question 2'
 print 3**27
 
 #question 3 (134533 + 6889980 -84849) to the power of 3.
+print 'question 3'
 print (134533+6889980-84849)**3
+
+'''question 4'''
+print 'question 4'
+'''convert km to meters'''
+print 'convert km to meters'
+print 7.9 * 1000
+
+#divide 45 by the answer
+print 'divide 45 by the answer'
+print 45.0/7900
+
+#wirte the answer in a sentence form
+print 'the average time per meter is 0.00569620253165 minutes'
+print 'question 5'
+
+
+'''question 5
+use the  Pythagorean theorem, to find the shortest distance
+so first you add the square of 4.7 and  the square of 9.4'''
+print'use the  Pythagorean theorem, to find the shortest distance so first you add the square of 4.7 and  the square of 9.4'
+print (4.7**2)+(9.4**2)
+
+#now find the square root of the answer
+print ' find the square root of the answer'
+print 110.45**1/2
+
+#convert it to miles by multiplying the answer by 0.621371
+print 'convert it to miles by multiplying the answer by 0.621371'
+print 55.225*0.621371
+
+#print it in a sentence
+print "the shortest distance between the two of us is 34.315213475 miles" 

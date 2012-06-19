b503222b189c8681bf7befee0011bae21af1328c
Nana Kwesi Baffoe-Mensah <NanaK@Nanas-MacBook-Pro-2.local>
2012-06-19 16:03:44 +0000

    :)

--- Day1.py
+++ Day1.py
@@ -1,2 +1,16 @@
 print'Nana Kwesi Baffoe Mensah'
 print'Hello World'
+
+
+#question 1 error
+
+print 11/2
+
+#question 1 correct answer
+print 11.0/2
+
+#question 2  exponentials
+print 3**27
+
+#question 3 (134533 + 6889980 -84849) to the power of 3.
+print (134533+6889980-84849)**3
